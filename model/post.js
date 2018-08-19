ddae033d87631668fa07c8ae7ab54335539d84c9
angelochua <angelo_chua@dlsu.edu.ph>
2018-08-20 01:45:15 +0800

    upload not working

--- model/post.js
+++ model/post.js
@@ -8,26 +8,33 @@ var postSchema = mongoose.Schema({
         minlength:3,
         trim : true
     },
+    
     description:{
         type:String,
         required: true,
         minlength:5
     },
+    
     photo:{
-        data: Buffer,
-        contentType:String
+        title : String,
+        filename : String,
+        originalfilename: String
     },
+    
     username:{
         users: [{type: mongoose.Schema.ObjectId, ref: 'User'}]
     },
+    
     status:{
         type: String,
         required: true
     },
+    
     sharedto:{
         type: String,
         minlength: 3
     },
+    
     tags:{
         tag: [{type: mongoose.Schema.ObjectId, ref:'Post'}]
     }
--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.use(express.static(__dirname + "/public/"))
 app.use(express.static(__dirname + "/views/"))
 app.use(express.static(__dirname + "/css/"))
 app.use(express.static(__dirname + "/memiges/"))
+app.use(express.static(__dirname + "/uploads/"))
 
 
 
@@ -38,6 +39,17 @@ app.use(express.static(__dirname + "/memiges/"))
 mongoose.Promise = global.Promise
 mongoose.connect("mongodb://localhost:27017/meme",{
     useNewUrlParser: true
+})
+
+
+/***** UPLOAD PATH *****/
+const UPLOAD_PATH = path.resolve(__dirname, "uploads")
+const upload = multer({
+  dest: UPLOAD_PATH,
+  limits: {
+    fileSize : 10000000,
+    files : 2
+  }
 })
 
 
@@ -312,6 +324,43 @@ app.post("/home-user", urlencoder, (req, res)=>{
     })
     })
    
+})
+
+// this should be in controller post
+// we use the multer middleware where
+// "img" must match the name attribute of the <input type="file">
+// we can access what multer saved through the req.file object
+// req.file.filename = name that multer assigned to the saved image
+// req.file.originalname = original name of the file from user's computer
+app.post("/upload", upload.single("img"),(req, res)=>{
+  console.log(req.body.title)
+  console.log(req.file.filename)
+  console.log(req.file.originalfilename)
+
+  // multer saves the actual image, and we save the filepath into our DB
+  var p = new Post({
+      title : req.body.title,
+      filename : req.file.filename,
+      originalfilename : req.file.originalname
+    })
+
+  p.save().then((doc)=>{
+      res.render("profile.hbs", {
+        title : doc.title,
+        id : doc._id
+      })
+    })
+})
+
+// this should be in controller post
+app.get("/photo/:id", (req, res)=>{
+  console.log(req.params.id)
+  Post.findOne({_id: req.params.id}).then((doc)=>{
+    fs.createReadStream(path.resolve(UPLOAD_PATH, doc.filename)).pipe(res)
+  }, (err)=>{
+    console.log(err)
+    res.sendStatus(404)
+  })
 })
 
 
